498365ba0b08c637cc3f15a5ef13a0d3d4379bf1
Jeremy Patterson <jrmypttrsn@gmail.com>
2016-04-04 22:12:56 -0500

    Added some routes

--- main.rb
+++ main.rb
@@ -2,3 +2,20 @@ require "pry"
 require "sinatra"
 
 # Your routes go here:
+get '/shouted-greeting/:name' do
+	"HELLO, #{params['name'].upcase}!"
+end
+
+get '/shouted-greeting/howdy/:name' do
+	"HOWDY, #{params['name'].upcase}!"
+end
+
+get '/shouted-greeting-b' do
+	if params['greeting']
+		"#{params[greeting].upcase}, #{params['name'].upcase}!"
+	else
+		"HELLO, #{params['name'].upcase}!"
+	end
+end
+
+get '/even-or-odd/2'
